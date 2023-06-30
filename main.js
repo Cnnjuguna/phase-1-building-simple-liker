6d074fec18206f5fb374cc292f3b1524f7731c55
njugudev <njugudev@pop-os.localdomain>
2023-06-30 07:23:29 +0300

    Completed assignment

--- main.js
+++ main.js
@@ -4,6 +4,45 @@ const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
 
+const unclickedHeart = document.querySelector(".like-glyph");
+
+// Target the error modal and message
+
+const errorModal = document.querySelector("modal");
+const errorMessage = document.querySelector("#modal-message");
+
+
+function toggleHeartState(heart) {
+  if (heart.innerHTML === EMPTY_HEART){heart.innerHTML = FULL_HEART
+    heart.classList.add('activated-heart');
+
+  } else 
+  { heart.innerHTML = EMPTY_HEART
+    heart.classList.remove('activated-heart');
+  }
+}
+
+
+document.addEventListener("DOMContentLoaded", (e) => {
+  const hearts = document.querySelectorAll(".like-glyph");
+
+  hearts.forEach(heart => {
+      heart.addEventListener("click", () => {
+       mimicServerCall()
+       .then(() =>{
+          toggleHeartState(heart);
+       })
+       .catch(() => {
+          errorMessage.innerHTML = error;
+          errorModal.classList.remove("hidden")
+          setTimeout(() => { errorModal.classList.add("hidden")}, 3000);
+          toggleHeartState(heart);
+       });
+      });
+    });
+})
+
+
 
 
 
@@ -20,6 +59,6 @@ function mimicServerCall(url="http://mimicServer.example.com", config={}) {
       } else {
         resolve("Pretend remote server notified of action!");
       }
-    }, 300);
+    }, 3000);
   });
 }
